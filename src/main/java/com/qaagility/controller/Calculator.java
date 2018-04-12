f220a7fa21e2600609c2777521c762484aa05dae
lakshmil20 <38283605+lakshmil20@users.noreply.github.com>
2018-04-12 15:30:20 +0530

    Update Calculator.java

--- src/main/java/com/qaagility/controller/Calculator.java
+++ src/main/java/com/qaagility/controller/Calculator.java
@@ -2,8 +2,10 @@ package com.qaagility.controller;
 
 public class Calculator {
 
-  public int add() {
-    return 3 + 6;
+  public int add(int x, int y) {
+    System.out.println("The real addmethod is called.");
+    result= x + y;
+    return result;
   
   }
   
